af42b7f7153f383adc2a95c2f18aaafcb3c78d03
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2024-07-06 00:22:53 +0800

    update

--- api/index.js
+++ api/index.js
@@ -135,7 +135,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.json({
+  res.send({
     message: "Welcome to ChatGPT API Playwright reverse proxy by Deviate",
   });
 });
